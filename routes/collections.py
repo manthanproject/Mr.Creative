f1fe1d232a12af089fa223ec47cc7d008d82f06c
manthanproject <manthanproject@users.noreply.github.com>
2026-05-13 13:12:40 +0530

    fix: null all 3 collection FKs before delete (Generation, SocialPost, AgentJob)

--- routes/collections.py
+++ routes/collections.py
@@ -188,9 +188,11 @@ def bulk_delete():
     for col_id in collection_ids:
         collection = Collection.query.filter_by(id=col_id, user_id=current_user.id).first()
         if collection:
-            # Null out FK on generations first (prevent FK constraint error)
-            from models import Generation
+            # Null out all FKs referencing this collection
+            from models import Generation, SocialPost, AgentJob
             Generation.query.filter_by(collection_id=col_id).update({'collection_id': None})
+            SocialPost.query.filter_by(collection_id=col_id).update({'collection_id': None})
+            AgentJob.query.filter_by(collection_id=col_id).update({'collection_id': None})
 
             # Delete collection folder
             col_dir = os.path.join(output_folder, f'collection_{col_id}')
